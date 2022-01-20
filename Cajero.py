f696bec173f39a100c55fed9a1ae0a4125b26d0c
pablo bracciale <pabloibracciale@gmail.com>
2022-01-20 18:30:02 -0300

    agregar contraseña

--- Cajero.py
+++ Cajero.py
@@ -8,9 +8,11 @@ This is a temporary script file.
 leersaldo = open('saldo.txt', 'r')
 saldo= int ((leersaldo.read()))
 leersaldo.close()
+leerclave=open('clave.txt', 'r')
+clave=leerclave.read()
+leerclave.close()
 
-
-preguntaInicial = int (input ("Que desea hacer: 1 Depositar 2 Retirar "))
+preguntaInicial = int (input ("Que desea hacer: 1 Depositar 2 Retirar 3 Modificar clave "))
 
 
 if preguntaInicial==1:
@@ -19,6 +21,7 @@ if preguntaInicial==1:
     grabarsaldo = open('saldo.txt', 'w')
     grabarsaldo.write(str(saldo))
     grabarsaldo.close()
+    print ("Su saldo ahora es de", saldo)
 if preguntaInicial==2:
     retiro = int (input ("Cuanto desea retirar? "))
     if saldo<retiro:
@@ -28,5 +31,18 @@ if preguntaInicial==2:
         grabarsaldo = open('saldo.txt', 'w')
         grabarsaldo.write(str(saldo))
         grabarsaldo.close()
-
+if preguntaInicial==3:
+    corroborar = input ("Ingrese su actual clave: ")
+    if corroborar==clave:
+        nuevaClave=input ("Ingrese nueva clave: ")
+        corroborarnuevaclave=input ("Ingrese nuevamente ")
+        if nuevaClave==corroborarnuevaclave:
+            clave=nuevaClave
+            grabarclave = open('clave.txt', 'w')
+            grabarclave.write(str(clave))
+            grabarclave.close()
+            print ("Ingreso correctamente nueva clave ")
+        else:
+            print ("No ingreso correctamente nueva clave")
+    
         
